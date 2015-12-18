5d27dc480492c670550394f968125be71873c39d
O L <O L>
2015-12-18 17:14:03 +0200

    t

--- ConsoleApplication1/ConsoleApplication1/DependGraph.c
+++ ConsoleApplication1/ConsoleApplication1/DependGraph.c
@@ -1,28 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
-#include <assert.h>
 #include "DependGraph.h"
+#include "Stack.h"
 
-typedef struct _DEPEND_GRAPH
+typedef struct DEPEND_GRAPH_
 {
 	PNODE head;
 	DELETE_ELEMENT DeleteF;
 	COMPARE_ELEMENT CompareF;
-	PRINT_ELEMENT PrintF;
+	PRINT_ELEMENT PrintE;
 	CLONE_ELEMENT CloneF;
 
 } DEPEND_GRAPH;
+
+typedef struct DependGraphNode_
+{
+	PElement pElem;
+	PNODE parentNode;
+	PNODE nextNode;
+	Bool was_executed;
+} NODE;
 
 /*
 * Private helper functions
 */
 
+/* *****************************************************
+Function: FindNode
+Abstract:  Searches for the node of a given element in a given graph
+Parameters:
+	pDG - pointer to the dependency graph
+	pElem - pointer to the element for finding its node
+Return Value:
+	pSearch - pointer to the requested node of element, or NULL if it's not on the dependency graph
+***************************************************** */
+PNODE FindNode(PDEPEND_GRAPH pDG, PElement pElem)
+{
+	PNODE pSearch = pDG->head;
+
+	for (pSearch = pDG->head; pSearch != NULL; pSearch = pSearch->nextNode) // Search if node already exists
+	{
+		if (pDG->CompareF(pSearch->pElem, pElem) == EQUAL)
+			return pSearch;
+	}
+
+	return NULL;
+}
+
 /*
 * Interface implementation
 */
 
-PDEPEND_GRAPH CreateGRAPH(CLONE_ELEMENT CloneF, DELETE_ELEMENT DeleteF, COMPARE_ELEMENT CompareF, PRINT_ELEMENT PrintF)
+/* *****************************************************
+Function: CreateGraph
+Abstract: Creates a new dependancy graph and initializes it
+Parameters:
+	CloneF - pointer to cloning element function
+	DeleteF - pointer to deleting element function
+	CompareF - pointer to comparing elements function
+	PrintE - pointer to printing element function
+Return Value:
+	pDG - pointer to the created dependency graph
+***************************************************** */
+PDEPEND_GRAPH CreateGraph(CLONE_ELEMENT CloneF, DELETE_ELEMENT DeleteF, COMPARE_ELEMENT CompareF, PRINT_ELEMENT PrintE)
 {
 	PDEPEND_GRAPH pDG;
 
@@ -33,33 +73,42 @@ PDEPEND_GRAPH CreateGRAPH(CLONE_ELEMENT CloneF, DELETE_ELEMENT DeleteF, COMPARE_
 		fprintf(stdout, "DependGraph.c: Failed to allocate memory");
 		exit(-1);
 	}
-
+	// graph initialization
 	pDG->head = NULL;
 	pDG->CompareF = CompareF;
 	pDG->DeleteF = DeleteF;
 	pDG->CloneF = CloneF;
-	pDG->PrintF = PrintF;
+	pDG->PrintE = PrintE;
 
 	return pDG;
 }
 
-void DeleteGRAPH(PDEPEND_GRAPH pDG)
+/* *****************************************************
+Function: DeleteGraph
+Abstract: Deletes a graph and its contents
+Parameters:
+	pDG - pointer to the desired dependency graph to delete
+Return Value: N/a
+***************************************************** */
+void DeleteGraph(PDEPEND_GRAPH pDG)
 {
 	PNODE pNode, pNext;
 	
-	if (pDG == NULL)
+	if (pDG == NULL) // no graph (parameters check)
 		return;
 
 	pNode = pDG->head;
 
-	if (pNode == NULL) {
+	if (pNode == NULL) // it's an empty graph
+	{
 		free(pDG);
 		return;
 	}
 
 	pNext = pNode->nextNode;
 
-	while (pNext != NULL) {
+	while (pNext != NULL) // delete all contents of graph
+	{
 		pDG->DeleteF(pNode->pElem);
 		free(pNode);
 		pNode = pNext;
@@ -67,91 +116,91 @@ void DeleteGRAPH(PDEPEND_GRAPH pDG)
 	
 	pDG->DeleteF(pNode->pElem);
 	free(pNode);
-	free(pDG);
+
+	free(pDG); // delete graph
+
+	return;
 }
 
+/* *****************************************************
+Function: AddElement
+Abstract: Adds an element to the dependency graph and links it to desired (existing) elements
+Parameters:
+	pDG - pointer to the dependency graph
+	pNewElement - pointer to the element for addition
+	pParentElement - pointer to the element which the new one should depend on
+Return Value:
+	FAILURE if basic requirements aren't met (no graph or element, element already exists or parent doesn't), otherwise SUCCESS
+***************************************************** */
 Result AddElement(PDEPEND_GRAPH pDG, PElement pNewElement, PElement pParentElement)
 {
 	PNODE pNewNode, pParentNode;
 
-	if (pDG == NULL || pNewElement == NULL) // no GRAPH or no ELEMENT to add (parameters check)
+	if (pDG == NULL || pNewElement == NULL) // no graph or no element to add (parameters check)
 		return FAILURE;
 
-	pNewNode = FindNode(pDG, pNewElement);
+	pNewNode = FindNode(pDG, pNewElement); // search if the new element already exists on graph
 
-	if (pNewNode != NULL) // node already exists
+	if (pNewNode != NULL)
 		return FAILURE;
 	
-	pParentNode = FindNode(pDG, pParentElement); // search Parent (lior saved it + flag.. for what ??)
+	pParentNode = FindNode(pDG, pParentElement); // search parent element's node
 
 	if (pParentElement != NULL && pParentNode == NULL) // parent element isn't NULL + its node doesn't exist on graph
 		return FAILURE;
 
-	pNewNode = (PNODE)malloc(sizeof(NODE)); // ADT creates a node and initialize + copies the element
+	pNewNode = (PNODE)malloc(sizeof(NODE)); // ADT creates a node and initializes + copies the element
 	if (pNewNode == NULL)
 	{
 		fprintf(stdout, "DependGraph.c: Failed to allocate memory");
 		exit(-1);
 	}
-
+	// new element's node initialization
 	pNewNode->pElem = pDG->CloneF(pNewElement); // copy the element
-	pNewNode->parentNode = pParentNode; // link to found (or NULL) 
-	pNewNode->nextNode = pDG->head; // add to start of list
+	pNewNode->parentNode = pParentNode; // link to found parent (or NULL)
+	pNewNode->nextNode = pDG->head;
 	pDG->head = pNewNode;
 	pNewNode->was_executed = FALSE;
 
 	return SUCCESS;
 }
 
-PNODE FindNode(PDEPEND_GRAPH pDG, PElement pElem)
-{
-	PNODE pSearch = pDG->head;
-
-	for (pSearch = pDG->head; pSearch != NULL; pSearch = pSearch->nextNode) // Search if NODE (key, elem) already exists
-	{
-		if (pDG->CompareF(pSearch->pElem, pElem) == EQUAL)
-			return pSearch;
-	}
-
-	return NULL;
-}
-
-
+/* *****************************************************
+Function: ExecuteElement
+Abstract: Marks required element as executed 
+Parameters:
+	pDG - pointer to the dependency graph
+	pElementExec - pointer to the element for execution
+Return Value:
+	FAILURE if basic requirements aren't met (no graph or element, element doesn't exist or already executed), otherwise SUCCESS
+***************************************************** */
 Result ExecuteElement(PDEPEND_GRAPH pDG, PElement pElementExec)
 {
-	PNODE pExecNode;
+	PNODE pExecNode, pTmpNode;
 
-	if (pDG == NULL || pElementExec == NULL) // parameters check
+	if (pDG == NULL || pElementExec == NULL) // no graph or no element to execute (parameters check)
 		return FAILURE;
 
-	pExecNode = FindNode(pDG, pElementExec); // find the node of input element
+	pExecNode = FindNode(pDG, pElementExec); // find the node of element for execution
 
-	if (pExecNode == NULL || pExecNode->was_executed == TRUE)
+	if (pExecNode == NULL || pExecNode->was_executed == TRUE) // didn't find the element or it's already executed
 		return FAILURE;
 
-	while (pExecNode->parentNode != NULL) // look up on graph
+	pTmpNode = pExecNode->parentNode; // go up on graph, starting from parent
+
+	while (pTmpNode != NULL)
 	{
-		if (pExecNode->parentNode->was_executed == FALSE) // if up node isn't executed...
+		if (pTmpNode->was_executed == FALSE) // if parent element isn't executed, the required element can't be executed too
 		{
 			printf("Error - Cannot execute :\n");
-			pDG->PrintF(pElementExec);
+			pDG->PrintE(pExecNode->pElem);
 			printf("The next Prerequisite is not satisfied :\n");
-			pDG->PrintF(pExecNode->parentNode->pElem);
+			pDG->PrintE(pTmpNode->pElem);
 
 			return FAILURE;
 		}
 			
-		pExecNode = pExecNode->parentNode;
-	}
-
-	if (pExecNode->was_executed == FALSE) // last iteration
-	{
-		printf("Error - Cannot execute :\n");
-		pDG->PrintF(pElementExec);
-		printf("The next Prerequisite is not satisfied :\n");
-		pDG->PrintF(pExecNode->parentNode->pElem);
-
-		return FAILURE;
+		pTmpNode = pTmpNode->parentNode;
 	}
 
 	pExecNode->was_executed = TRUE;
@@ -159,18 +208,25 @@ Result ExecuteElement(PDEPEND_GRAPH pDG, PElement pElementExec)
 	return SUCCESS;
 }
 
+/* *****************************************************
+Function: PrintExecElements
+Abstract: Prints all of executed elements, by order of chronological addition
+Parameters: 
+	pDG - pointer to the dependency graph
+Return Value: N/a
+***************************************************** */
 void PrintExecElements(PDEPEND_GRAPH pDG)
 {
 	PNODE pNode;
 	PStack pStack;
 	Result pushresult;
 
-	if (pDG == NULL)
+	if (pDG == NULL) // no graph
 		return;
 
 	pNode = pDG->head;
 
-	pStack = StackCreate(pDG->CloneF, pDG->PrintF, pDG->DeleteF);
+	pStack = StackCreate(pDG->CloneF, pDG->PrintE, pDG->DeleteF);
 
 	if (pStack == NULL)
 	{
@@ -199,12 +255,19 @@ void PrintExecElements(PDEPEND_GRAPH pDG)
 	return;
 }
 
+/* *****************************************************
+Function: CountElements
+Abstract: Counts the number of elements on the graph
+Parameters:
+	pDG - pointer to the dependency graph
+Return Value: N/a
+***************************************************** */
 int CountElements(PDEPEND_GRAPH pDG)
 {
 	PNODE pNode;
 	int counter = 0;
 
-	if (pDG == NULL)
+	if (pDG == NULL) // no graph
 		return -1;
 
 	pNode = pDG->head;
@@ -218,18 +281,29 @@ int CountElements(PDEPEND_GRAPH pDG)
 	return counter;
 }
 
+/* *****************************************************
+Function: PrintPrerequisities
+Abstract: Prints all prerequisities' elements of a given element, including it
+Parameters:
+	pDG - pointer to the dependency graph
+	pElem - pointer to the required element
+Return Value: N/a
+***************************************************** */
 void PrintPrerequisities(PDEPEND_GRAPH pDG, PElement pElem)
 {
 	PNODE pNode;
 	PStack pStack;
 	Result pushresult;
 
-	if (pDG == NULL || pElem == NULL)
+	if (pDG == NULL || pElem == NULL) // no graph or element
 		return;
 
-	pNode = pDG->head;
+	pNode = FindNode(pDG, pElem); // find required element's node
 
-	pStack = StackCreate(pDG->CloneF, pDG->PrintF, pDG->DeleteF);
+	if (pNode == NULL) // didn't find it
+		return;
+
+	pStack = StackCreate(pDG->CloneF, pDG->PrintE, pDG->DeleteF);
 
 	if (pStack == NULL)
 	{
@@ -237,56 +311,102 @@ void PrintPrerequisities(PDEPEND_GRAPH pDG, PElement pElem)
 		exit(-1);
 	}
 
-	while (p->parentNode != NULL)
+	while (pNode != NULL)
 	{
-		// push
-		if (StackPush(pStack, p->pElem) == FAILURE)
-			return;
-		p = p->parentNode;
+		pushresult = StackPush(pStack, pNode->pElem);
+
+		if (pushresult == FAILURE)
+		{
+			fprintf(stdout, "DependGraph.c: Failed to allocate memory");
+			exit(-1);
+		}
+
+		pNode = pNode->parentNode;
 	}
 
-	void StackPrint(pStack);
+	StackPrint(pStack);
+	StackDestroy(pStack);
 
+	return;
 }
 
-Result ResetGRAPH(PDEPEND_GRAPH pDG) {
+/* *****************************************************
+Function: ResetGRAPH
+Abstract: Marks all elements as not executed
+Parameters:
+	pDG - pointer to the dependency graph
+Return Value:
+	FAILURE if graph's existance requirement isn't met, otherwise SUCCESS
+***************************************************** */
+Result ResetGRAPH(PDEPEND_GRAPH pDG)
+{
+	PNODE pNode;
 
-	PNODE p = pDG->head;
-
-	if (pDG == NULL)
+	if (pDG == NULL) // no graph
 		return FAILURE;
 
-	while (p->nextNode != NULL) {
-		p->was_executed = 0;
-		p = p->nextNode;
-	}
+	pNode = pDG->head;
 
-	p->was_executed = 0;
+	while (pNode != NULL)
+	{
+		pNode->was_executed = FALSE;
+		pNode = pNode->nextNode;
+	}
 
 	return SUCCESS;
 }
 
-Result RemoveGRAPH(PDEPEND_GRAPH pDG, PNODE pNode) {
-	
-	if (pNode == NULL || pDG == NULL)
+/* *****************************************************
+Function: RemoveElement
+Abstract: Removes an element's node from the graph, and links its dependencies to its parent
+Parameters:
+	pDG - pointer to the dependency graph
+	pElem - pointer to the element for removal
+Return Value:
+	FAILURE if basic requirements aren't met (no graph or element, element doesn't exist), otherwise SUCCESS
+***************************************************** */
+Result RemoveElement(PDEPEND_GRAPH pDG, PElement pElem)
+{
+	PNODE pCurr, pPrev, pSon;
+
+	if (pDG == NULL || pElem == NULL) // no graph or element to remove (parameters check)
 		return FAILURE;
 
-	PNODE p = pDG->head;
-	PNODE pSon = NULL;
-	PNODE pPrev = NULL;
+	pCurr = FindNode(pDG, pElem); // find element's node and save it
 
-	while (p->nextNode != NULL && (pSon == NULL || pPrev == NULL)) {
+	if (pCurr == NULL) // didn't find it (or empty graph)
+		return FAILURE;
 
-		if (p->parentNode == pNode) // compare keys?
-			pSon = p;
-		if (p->nextNode == pNode)
-			pPrev = p;
+	if (CountElements(pDG) == 1) // found it and there's only one node => it's the only one on graph
+	{
+		pDG->DeleteF(pCurr->pElem); // remove Element
+		free(pCurr); //free Node
 
-		p = p->nextNode;
+		pDG->head = NULL; // now an empty list
+
+		return SUCCESS;
 	}
 
-	pSon->parentNode = pNode->parentNode;
-	pPrev->nextNode = pNode->nextNode;
+	pPrev = pDG->head;
+	pSon = pDG->head;
+
+	while (pPrev->nextNode != pCurr) // save previous node
+		pPrev = pPrev->nextNode;
+
+	while (pSon != pCurr) // find all dependencies
+	{
+		if (pSon->parentNode == pCurr)
+		{
+			pSon->parentNode = pCurr->parentNode; // link every dependent node to current's parent
+		}
+
+		pSon = pSon->nextNode;
+	}
+
+	pPrev = pCurr->nextNode;
+
+	pDG->DeleteF(pCurr->pElem); // remove element
+	free(pCurr); // remove node
 
 	return SUCCESS;
 }
