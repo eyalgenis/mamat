96dedd5c90d03216141e37b1fe6d70d92133a387
eyalgenis <eyal.genis@gmail.com>
2015-12-18 17:23:51 +0200

    t

--- ConsoleApplication1/ConsoleApplication1/main.c
+++ ConsoleApplication1/ConsoleApplication1/main.c
@@ -12,14 +12,13 @@ typedef struct Course_ {
   char* name;
   char* ID;
   int points;
-  
 } Course, *PCourse;
 
-void Print_Course(Course*);
-Course* Clone_Course(Course*);
-void Delete_Course(Course*);
-CompareResult Compare_courses(Course*, Course*);
-
+void PrintCourse(Course*);
+Course* CloneCourse(Course*);
+void DeleteCourse(Course*);
+CompareResult CompareCourses(Course*, Course*);
+Course* CreateCourse(char*, char*, int);
 
 int main()
 {
@@ -27,6 +26,12 @@ int main()
   char szLine[MAX_LINE_SIZE];
   char* delimiters = " \t\n";
   char* pszCommand;
+
+  char* pID = NULL;
+  char* pName = NULL;
+  char* pPrerequisiteID = NULL;
+  char* pPoints;
+  int points;
    
 
   while (fgets(szLine, MAX_LINE_SIZE, stdin))
@@ -40,17 +45,27 @@ int main()
         }
       if ( 0 == strcmp(pszCommand, "Add_Course") )
         {
-	  
-	  /*
-	    Add your code here
-	   */
+			
+
+			pID = strtok(NULL, delimiters);
+			pName = strtok(NULL, delimiters);
+			pPoints = strtok(NULL, delimiters);
+			pPrerequisiteID = strtok(NULL, delimiters);
+
+			if (pID == NULL || pName == NULL || pPoints == NULL || pPrerequisiteID == NULL) {
+				fprintf(stderr, "%s failed: not enough parameters\n", pszCommand);
+			}
+
+			
+			points = (atoi)(pPoints);
+
+			Course* CreateCourse(char* ID, char* name, int points)
+
 	}
       
       else if ( 0 == strcmp(pszCommand, "Print_Prerequisites") )
 	{
-	  /*
-	    Add your code here
-	  */
+		
 	}
       else if ( 0 == strcmp(pszCommand, "Print_Executed") )
 	{
@@ -94,29 +109,75 @@ int main()
   return 0;
 }
 
-void Print_Course(Course* course) {
-	printf("Course - <%c>\n", &course->ID);
+Course* CloneCourse(Course* course) {
+
+	Course* pCourse;
+	
+	pCourse = (Course*)malloc(sizeof(Course));
+
+	if (pCourse == NULL) {
+		fprintf(stdout, "main.c: Failed to allocate memory");
+		exit(-1);
+	}
+
+	pCourse->name = (char*)malloc(strlen(course->name) + 1);
+	pCourse->ID = (char*)malloc(strlen(course->ID) + 1);
+
+	if (pCourse->name == NULL || pCourse->ID == NULL)
+	{
+		fprintf(stdout, "main.c: Failed to allocate memory");
+		exit(-1);
+	}
+
+	strcpy(pCourse->name, course->name);
+	strcpy(pCourse->ID, course->ID);
+	pCourse->points = course->points;
+
+	return pCourse;
+
+}
+void PrintCourse(Course* course) {
+
+	printf("Course - %s\n", course->ID);
 	printf("====================\n");
-	printf("Name - <%c>\n", &course->name);
-	printf("Name - <%d>\n", &course->points);
-
+	printf("Name - %s\n", course->name);
+	printf("Name - %d", course->points);
 }
-
-
-Course* Clone_Course(Course* course1) {
-
-	Course* course2 malloc(sizeof)
-
-		return course2;
-
+void DeleteCourse(Course* course) {
+	free(course->ID);
+	free(course->name);
+	free(course);
 }
+Course* CreateCourse(char* ID, char* name, int points) {
 
+	Course* pCourse;
+	pCourse = (Course*)malloc(sizeof(Course));
+	
+	if (pCourse == NULL) {
+		fprintf(stdout, "main.c: Failed to allocate memory");
+		exit(-1);
+	}
 
+	pCourse->name = (char*)malloc(strlen(name) + 1);
+	pCourse->ID = (char*)malloc(strlen(ID) + 1);
+	
+	if (pCourse->name == NULL || pCourse->ID == NULL) {
+			fprintf(stdout, "main.c: Failed to allocate memory");
+			exit(-1);
+		}
 
+	strcpy(pCourse->name, name);
+	strcpy(pCourse->ID, ID);
+	pCourse->points = points;
 
+	return pCourse;
+}
+CompareResult CompareCourses(Course* course1, Course* course2) {
+	
+	if (strcmp(course1->ID, course2->ID) == 0) return EQUAL;
+	if (course1 == NULL && course2 == NULL) return EQUAL;
 
-
-
-
+	return NOT_EQUAL;
+}
 
 
