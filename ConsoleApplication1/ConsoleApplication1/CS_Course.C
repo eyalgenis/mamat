65696e5921d8bfb9439c69ce5c39cd5028fe31f8
eyalgenis <eyal.genis@gmail.com>
2016-01-03 19:20:03 +0200

    text

---
+++ ConsoleApplication1/ConsoleApplication1/CS_Course.C
@@ -0,0 +1,40 @@
+#include "CS_Course.H"
+
+CS_Course :: CS_Course(int hwTakef) : 
+	Course(), hwTakef_(hwTakef) {}
+
+bool CS_Course::isTakef() const
+{
+	return hwTakef_;
+}
+
+char * CS_Course::getBook() const
+{
+	return bookName_;
+}
+
+bool CS_Course::setTakef(bool takef)
+{
+	hwTakef_ = takef;
+	return 1;
+}
+
+char * CS_Course::setbook(char * book)
+{ 
+	// destroy exiting data
+	delete[] bookName_;
+	// create new data
+	bookName_ = new char[strlen(book) + 1];
+	return strcpy(bookName_, book);
+}
+
+int CS_Course::getCourseGrade() const
+{
+	int finalGrade;
+	if (hwTakef_)
+		finalGrade = round((1 - hwWeigh_)*examGrade_ + hwWeigh_*getHwAverage());
+	if (!hwTakef_)
+		finalGrade = max(round((1 - hwWeigh_)*examGrade_ + hwWeigh_*getHwAverage()), examGrade_);
+
+	return finalGrade;
+}
--- ConsoleApplication1/ConsoleApplication1/Defines.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DEFINES__
-#define __DEFINES__
-
-typedef enum {FAILURE,SUCCESS} Result;
-typedef enum {EQUAL,NOT_EQUAL} CompareResult;
-typedef enum {FALSE,TRUE} Bool;
-
-#endif
