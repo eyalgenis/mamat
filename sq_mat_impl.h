bdaa271d28f884054ed0b5699126be3b0ad024c0
eyalgenis <eyal.genis@gmail.com>
2016-01-20 20:45:37 +0200

    text

--- sq_mat_impl.h
+++ sq_mat_impl.h
@@ -1,49 +1,70 @@
 #ifndef _SQ_MAT_IMPL_H_
 #define _SQ_MAT_IMPL_H_
 
-#include <math.h>
+#include <cmath>
 #include "sq_mat.h"
 #include "mat.h"
 
+//*******************************************************************************************
+// Name:           SqMat<T>::SqMat(Mat<T> m)
+// Description:    Constructs a new square matrix with Mat<T> m elements and properties
+// Parameters:     Mat<T> m
+// Return value:   
+//*******************************************************************************************
 template<class T>
 SqMat<T>::SqMat(Mat<T> m) : Mat<T>(m)
 {
 	ExceptionWrongDimensions e_wrong_dim;
-	if (m.width() != m.height())
+	if (m.width() != m.height()) // check matrix width & height equality
 		throw(e_wrong_dim);
 }
 
+//*******************************************************************************************
+// Name:           SqMat<T>::size() const
+// Description:    Calculates the square matrix size
+// Parameters:     
+// Return value:   unsigned int w_
+//*******************************************************************************************
 template<class T>
 unsigned int SqMat<T>::size() const
 {
-	unsigned int w = this->w_;
-	return w;
+	return this->w_; // using just width because width=height
 }
 
+//*******************************************************************************************
+// Name:           SqMat<T>::determinant() const
+// Description:    Calculates the square matrix determinant with recursion
+// Parameters:     
+// Return value:   T det
+//*******************************************************************************************
 template<class T>
 T SqMat<T>::determinant() const
 {
 	T det = 0;
 	unsigned int p, h, k, i, j;
-	Vec<T> line_vec, empty_vec;
 	unsigned int n = this->size();
+	Vec<T> line_vec, empty_vec;
 
-	Mat<T> mat_temp(this->size() - 1);
+	Mat<T> mat_temp(this->size() - 1); // create temp new smaller matrix to calculate its determinant (recursion)
 	
-	for (unsigned int l = 0; l < n - 1; l++)
-	{
-		for (unsigned int t = 0; t < n - 1; t++)
-		{
+	unsigned int l = 0;
+	while (l < n - 1) { // copy this square matrix to the temp matrix
+
+		unsigned int t = 0;
+		while (t < n - 1) {
 			line_vec.push_back(0);
+			t++;
 		}
+
 		mat_temp.push_back(line_vec);
 		line_vec = empty_vec;
+		l++;
 	}
 
-	if (n == 1) {
+	if (n == 1) { // stoppage condition 1 - no calculation for 1x1 matrix
 		return (*this)[0][0];
 	}
-	else if (n == 2) {
+	else if (n == 2) { // stoppage condition 2 - short calculation of 2x2 matrix)
 		det = ((*this)[0][0] * (*this)[1][1] - (*this)[0][1] * (*this)[1][0]);
 		return det;
 	}
@@ -64,14 +85,12 @@ T SqMat<T>::determinant() const
 					}
 				}
 			}
-			SqMat<T> sq_mat_temp = (SqMat<T>) mat_temp;
-			det = det + (*this)[0][p] * pow(-1, p) * sq_mat_temp.determinant();
+			SqMat<T> sq_mat_temp = (SqMat<T>) mat_temp; // casting for temp matrix to square matrix - needed to calculate determinant
+			det = det + (*this)[0][p] * pow(-1, p) * sq_mat_temp.determinant(); // recursion
 		
 		}
 		return det;
 	}
 }
-
-
 
 #endif // _SQ_MAT_IMPL_H_
--- vec_impl.h
+++ vec_impl.h
@@ -5,54 +5,96 @@
 #include <iostream>
 #include <cmath>
 
+//********************************************************************************************
+// Name:           ExceptionWrongDimensions::what()
+// Description:    dimenesions of operands don't match - illegal operation
+// Parameters:     
+// Return value:   
+//********************************************************************************************
 const char* ExceptionWrongDimensions::what() const throw()
 {
 	return "operand dimensions must agree";
 }
 
+//********************************************************************************************
+// Name:           ExceptionEmptyOperand::what()
+// Description:    this is an empty operand
+// Parameters:     
+// Return value:   
+//********************************************************************************************
 const char* ExceptionEmptyOperand::what() const throw()
 {
 	return "empty operand";
 }
 
+//********************************************************************************************
+// Name:           ExceptionIndexExceed::what()
+// Description:    access to index is illegal, index is out of definition
+// Parameters:     
+// Return value:   
+//********************************************************************************************
 const char* ExceptionIndexExceed::what() const throw()
 {
 	return "index exeeds operand dimensions";
 }
 
+//********************************************************************************************
+// Name:           Vec<T>::Vec(const T& el) constructor
+// Description:    initilize a vector with an element
+// Parameters:     const T& el
+// Return value:   
+//********************************************************************************************
 template<class T>
 Vec<T>::Vec(const T & el)
 {
 	this->vals_.push_back(el);
 }
 
+//********************************************************************************************
+// Name:           Vec<T>::push_back(T el)
+// Description:    add element to the end of vector
+// Parameters:     T el
+// Return value:   
+//********************************************************************************************
 template<class T>
 void Vec<T>::push_back(T el)
 {
 	vals_.push_back(el);
 }
 
+//********************************************************************************************
+// Name:           unsigned int Vec<T>::size() const
+// Description:    Calculates size of the vector
+// Parameters:    
+// Return value:   unsigned int vals_.size()
+//********************************************************************************************
 template<class T>
 unsigned int Vec<T>::size() const
 {
 	return vals_.size();
 }
 
+//********************************************************************************************
+// Name:           Vec<T>::operator+(const Vec & rhs) const
+// Description:    Sum this vector with with rhs vector
+// Parameters:     const Vec& rhs
+// Return value:   Vec<T> new_vec
+//********************************************************************************************
 template<class T>
 Vec<T> Vec<T>::operator+(const Vec & rhs) const
 {
-	ExceptionWrongDimensions e_wng_dim;
+	ExceptionWrongDimensions e_dimensions;
 	ExceptionEmptyOperand e_empty;
 	
 	Vec new_vec;
-	if (this->vals_.size() != rhs.size())
-		throw (e_wng_dim);
-	if (this->vals_.size() == 0)
+	if (this->vals_.size() != rhs.size()) // check vectors size equality
+		throw (e_dimensions);
+	if (this->vals_.size() == 0) // check not empty vector
 		throw (e_empty);
 	
 	unsigned int i = 0;
 	
-	while (i < this->vals_.size()) {
+	while (i < this->vals_.size()) { // sum & push till end of vector rechead
 		new_vec.push_back((*this)[i] + rhs[i]);
 		i++;
 	}
@@ -60,20 +102,26 @@ Vec<T> Vec<T>::operator+(const Vec & rhs) const
 	return new_vec;
 }
 
+//********************************************************************************************
+// Name:           Vec<T>::operator*(const T & rhs) const
+// Description:    Multiply this vector with a constant rhs
+// Parameters:     const T & rhs
+// Return value:   Vec<T> new_vec
+//********************************************************************************************
 template<class T>
 Vec<T> Vec<T>::operator*(const T & rhs) const
 {
 	ExceptionEmptyOperand e_empty;
 
+	if (this->size() == 0) // check not empty vector
+		throw (e_empty); 
+	
 	Vec<T> new_vec;
-
-	//if (rhs.size() == 0)
-	//	throw (e_empty);
 
 	unsigned int i = 0;
 	typename list<T>::const_iterator iter = this->begin();
 
-	while (i < this->size()) {
+	while (i < this->size()) { // multiply & push till end of vector rechead
 		new_vec.push_back(rhs * (*iter));
 		i++;
 		iter++;
@@ -82,58 +130,76 @@ Vec<T> Vec<T>::operator*(const T & rhs) const
 	return new_vec;
 }
 
+//********************************************************************************************
+// Name:           T & Vec<T>::operator[](unsigned int ind)
+// Description:    Reach element in "ind" position in vector
+// Parameters:     unsigned int ind
+// Return value:   T& Element in ind position
+//********************************************************************************************
 template<class T>
 T & Vec<T>::operator[](unsigned int ind)
 {
-	ExceptionIndexExceed e_idx_exc;
+	ExceptionIndexExceed e_exceed;
 	if (ind >= this->vals_.size())
-		throw(e_idx_exc);
+		throw(e_exceed);
 	
-	unsigned int i = 0;
-	list<T>::const_iterator iter = vals_.begin();
-	while (i < ind) {
-		i++;
+	list<T>::iterator iter = vals_.begin();
+
+	while (0 < ind) {
 		iter++;
+		ind--;
 	}
 
-	return const_cast<T&>(*iter);
+	return (*iter);
 }
 
+//********************************************************************************************
+// Name:           const T & Vec<T>::operator[](unsigned int ind) const
+// Description:    Reach element in "ind" position in vector
+// Parameters:     unsigned int ind
+// Return value:   const T& Element in ind position
+//********************************************************************************************
 template<class T>
 const T & Vec<T>::operator[](unsigned int ind) const
 {
-	ExceptionIndexExceed e_idx_exc;
+	ExceptionIndexExceed e_exceed;
 	if (ind >= this->vals_.size())
-		throw(e_idx_exc);
+		throw(e_exceed);
 
-	unsigned int i = 0;
 	list<T>::const_iterator iter = vals_.begin();
-	while (i < ind) {
-		i++;
+
+	while (0 < ind) {
 		iter++;
+		ind--;
 	}
 
-	return *iter;
+	return (*iter);
 }
 
+//********************************************************************************************
+// Name:           Vec<T>::operator,(const Vec & rhs) const
+// Description:    Concatanates this vector with rhs vector    
+// Parameters:     const Vec & rhs
+// Return value:   Vec<T> new_vec
+//********************************************************************************************
 template<class T>
 Vec<T> Vec<T>::operator,(const Vec & rhs) const
 {
 	ExceptionEmptyOperand e_empty;
-	if (rhs.size() == 0)
+	if (rhs.size() == 0) // check not empty vector
 		throw (e_empty);
 
 	Vec<T> new_vec;
 
 	typename list<T>::const_iterator iter = this->begin();
 	
-	while (iter != this->end()) {
+	while (iter != this->end()) { // push back this vector to new vector
 		new_vec.push_back(*iter);
 		iter++;
 	}
 
 	iter = rhs.begin();
-	while (iter != rhs.end()) {
+	while (iter != rhs.end()) { // push back rhs vector to new vector
 		new_vec.push_back(*iter);
 		iter++;
 	}
@@ -141,26 +207,41 @@ Vec<T> Vec<T>::operator,(const Vec & rhs) const
 	return new_vec;
 }
 
+//********************************************************************************************
+// Name:           Vec<T>::operator,(const Vec & rhs) const
+// Description:    Reach elements by "ind"s position given in vector and concatanates them to new vector
+// Parameters:     const Vec<unsigned int>& ind
+// Return value:   Vec<T> new_vec
+//********************************************************************************************
 template<class T>
 Vec<T> Vec<T>::operator[](const Vec<unsigned int>& ind) const
 {
-	ExceptionIndexExceed e_idx_exc;
+	ExceptionIndexExceed e_exceed;
+	ExceptionEmptyOperand e_empty;
+
+	if (ind.size() == 0) // check not empty vector
+		throw (e_empty);
 
 	Vec<T> new_vec;
 
-	unsigned int i=0;
-	while (i < ind.size()) {
-		if (ind[i] >= this->size())
-			throw (e_idx_exc);
+	unsigned int i = ind.size();
+	while (0 < i) { // reach & push back till end of indexes vector rechead
+		if (ind[i] >= this->size()) // check given index not exceeded vector size
+			throw (e_exceed);
 
 		new_vec.push_back((*this)[ind[i]]);
-		i++;
+		i--;
 	}
 
 	return new_vec;
 }
 
-
+//********************************************************************************************
+// Name:           Vec<T> operator*(const T & lhs, const Vec<T>& rhs)
+// Description:    Multiply rhs vector with lhs factor
+// Parameters:     const T & lhs, const Vec<T>& rhs
+// Return value:   Vec<T> new_vec
+//********************************************************************************************
 template<class T>
 Vec<T> operator*(const T & lhs, const Vec<T>& rhs)
 {
@@ -168,73 +249,90 @@ Vec<T> operator*(const T & lhs, const Vec<T>& rhs)
 
 	Vec<T> new_vec;
 
-	if (rhs.size() == 0)
+	if (rhs.size() == 0) // check not empty vector
 		throw (e_empty);
 
-	unsigned int i = 0;
+	unsigned int i = rhs.size();
 	list<T>::const_iterator iter = rhs.begin();
 
-	while (i < rhs.size()) {
+	while (0 < i) { // multiply & push back till end of vector
 		new_vec.push_back(lhs * (*iter));
-		i++;
 		iter++;
+		i--;
 	}
 
 	return new_vec;
 
 }
 
+//********************************************************************************************
+// Name:           ostream & operator<<(ostream & ro, const Vec<T>& v)
+// Description:    prints the vector by overloading << operator
+// Parameters:     ostream & ro, const Vec<T>& v
+// Return value:   ostream & ro
+//********************************************************************************************
 template<class T>
 ostream & operator<<(ostream & ro, const Vec<T>& v)
 {
-	ExceptionEmptyOperand e_empty_op;
-	if (v.size() == 0)
-	{
-		throw (e_empty_op);// ExceptionEmptyOperand;
-	}
-	typename list<T>::const_iterator it;
-	ro << "(" << *v.begin();
-	it = v.begin();
-	++it;
-	for (it = it; it != v.end(); it++)
-	{
-		ro << ",\t" << *it;
+	ExceptionEmptyOperand e_empty;
+
+	if (v.size() == 0) // check not empty vector
+		throw (e_empty);
+	
+	typename list<T>::const_iterator iter = v.begin();;
+
+	ro << "(" << *iter;
+	iter++;
+
+	while (iter != v.end()) { // print till end of vector
+		ro << ",\t" << *iter;
+		iter++;
 	}
 	ro << ")";
 	return ro;
 }
 
-
+//********************************************************************************************
+// Name:           Vec<T> range(T start, unsigned int size)
+// Description:    Creates new vector with n size (start , start+1, start+2 ... start+size-1)
+// Parameters:     T start, unsigned int size
+// Return value:   Vec<T> new_vec
+//********************************************************************************************
 template<class T>
 Vec<T> range(T start, unsigned int size)
 {
 	Vec<T> new_vec;
 
-	unsigned int i = 0;
-	while (i < size) {
+	while (0 < size) { // push back till vector size reached
 		new_vec.push_back(start);
 		start = start+1;
-		i++;
+		size--;
 	}
 
 	return new_vec;
 }
 
+//********************************************************************************************
+// Name:           bool operator()(Vec<T>& lhs, Vec<T>& rhs) const
+// Description:    Compare betweem lhs vector & rhs vector by infinity normal
+// Parameters:     Vec<T>& lhs, Vec<T>& rhs
+// Return value:   bool (lhs_norm < rhs_norm);
+//********************************************************************************************
 template<class T>
 class norm_inf_comp {
 public:
 	bool operator()(Vec<T>& lhs, Vec<T>& rhs) const {
 
-		ExceptionEmptyOperand e_empty_op;
-		if (lhs.size() == 0 || rhs.size() == 0)
-			throw (e_empty_op);	// ExceptionEmptyOperand;
+		ExceptionEmptyOperand e_empty;
+		if (lhs.size() == 0 || rhs.size() == 0) // check not empty vectors
+			throw (e_empty);	
 
 		typename list<T>::const_iterator iter = lhs.begin();
 
 		double lhs_norm = 0;
 		double rhs_norm = 0;
 
-		while (iter != lhs.end()) {
+		while (iter != lhs.end()) { // calculate infinity normal for lhs
 			if (lhs_norm < (double)modulu(*iter))
 				lhs_norm = (double)modulu(*iter);
 			iter++;
@@ -242,7 +340,7 @@ public:
 
 		iter = rhs.begin();
 
-		while (iter != rhs.end()) {
+		while (iter != rhs.end()) { // calculate infinity normal for rhs
 			if (rhs_norm < (double)modulu(*iter))
 				rhs_norm = (double)modulu(*iter);
 			iter++;
@@ -251,6 +349,5 @@ public:
 		return (lhs_norm < rhs_norm);
 	}
 };
-
 
 #endif // _VEC_IMPL_H_
